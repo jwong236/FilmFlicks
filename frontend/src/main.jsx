b73bb623ba97fc4657f11c0574ca4a30d149b14c
Jacob Wong <jacobsunsetbluff@gmail.com>
2024-04-10 20:39:26 -0700

    Implement example endpoint for practice

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx'
 import MovieList from '../src/pages/MovieList.jsx'
 import SingleStar from '../src/pages/SingleStar.jsx'
+import SingleMovie from '../src/pages/SingleMovie.jsx'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import './index.css'
 
@@ -13,6 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
           <Routes>
               <Route exact path="/" element={<MovieList/>} />
               <Route exact path="/movielist" element={<MovieList/>} />
+              <Route exact path="singlemovie" element={<SingleMovie/>} />
               <Route path="/singlestar" element={<SingleStar/>} />
 
           </Routes>
---
+++ frontend/src/pages/SingleMovie.jsx
@@ -0,0 +1,74 @@
+import {useEffect, useState} from 'react'
+import {Link, useLocation} from "react-router-dom";
+
+function SingleMovie() {
+    const [starData, setStarData] = useState({
+        name: "",
+        birthYear: "N/A",
+        movieTitles: []
+
+    });
+
+    const location = useLocation();
+    const search_params = new URLSearchParams(location.search);
+    const name = search_params.get('name');
+    //console.log(name);
+
+    useEffect(() => {
+        let mounted = true;
+        async function fetchStarData(){
+            try{
+                const response = await fetch(`http://localhost:8080/fabFlix_war/singlestar?name=${encodeURIComponent(name)}`);
+
+                if (!response.ok) {
+                    console.error('response is not status 200');
+                }
+
+
+                const data = await response.json();
+
+
+                if (mounted){
+                    setStarData(data[0]);
+                }
+
+
+
+            }catch(error){
+                console.error('Error fetching data: ', error);
+            }
+        }
+
+        fetchStarData();
+
+        return ()=>{
+            mounted = false;
+        }
+    }, []);
+
+
+    console.log("name " + starData.name);
+    console.log("birthyear " + starData.birthYear);
+    const birthYear = starData.birthYear === null ? "N/A" : starData.birthYear;
+    console.log(birthYear);
+    return (
+
+        <>
+            <div>
+                <Link to={"/"}>
+                    HOME
+                </Link>
+            </div>
+            <h1>
+                STAR
+            </h1>
+
+            <div>NAME: {starData.name}</div>
+            <div>YEAR: {birthYear}</div>
+            <div>MOVIES: {starData.movieTitles}</div>
+        </>
+    );
+
+}
+
+export default SingleMovie
