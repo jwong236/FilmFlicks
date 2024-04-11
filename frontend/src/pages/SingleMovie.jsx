162f566f505d04db03dd2a11eb828d4cfb456636
Jacob Wong <jacobsunsetbluff@gmail.com>
2024-04-10 22:53:35 -0700

    Partially Implement SingleMovie page for testing

--- frontend/src/pages/SingleMovie.jsx
+++ frontend/src/pages/SingleMovie.jsx
@@ -2,55 +2,43 @@ import {useEffect, useState} from 'react'
 import {Link, useLocation} from "react-router-dom";
 
 function SingleMovie() {
-    const [starData, setStarData] = useState({
-        name: "",
-        birthYear: "N/A",
-        movieTitles: []
+    const [movieData, setMovieData] = useState({
+        id: "0",
+        title: "default title",
+        year: "default year",
+        director: "default director"
 
     });
 
     const location = useLocation();
     const search_params = new URLSearchParams(location.search);
-    const name = search_params.get('name');
-    //console.log(name);
+    const title = search_params.get('title');
 
     useEffect(() => {
         let mounted = true;
-        async function fetchStarData(){
+        async function fetchMovieData(){
             try{
-                const response = await fetch(`http://localhost:8080/fabFlix_war/singlestar?name=${encodeURIComponent(name)}`);
+                const response = await fetch(`http://localhost:8080/fabFlix_war/singlemovie?title=${encodeURIComponent(title)}`);
 
                 if (!response.ok) {
                     console.error('response is not status 200');
                 }
-
-
                 const data = await response.json();
-
-
                 if (mounted){
-                    setStarData(data[0]);
+                    setMovieData(data[0]);
                 }
-
-
-
             }catch(error){
                 console.error('Error fetching data: ', error);
             }
         }
 
-        fetchStarData();
+        fetchMovieData();
 
         return ()=>{
             mounted = false;
         }
     }, []);
 
-
-    console.log("name " + starData.name);
-    console.log("birthyear " + starData.birthYear);
-    const birthYear = starData.birthYear === null ? "N/A" : starData.birthYear;
-    console.log(birthYear);
     return (
 
         <>
@@ -60,12 +48,12 @@ function SingleMovie() {
                 </Link>
             </div>
             <h1>
-                STAR
+                {movieData.title}
             </h1>
 
-            <div>NAME: {starData.name}</div>
-            <div>YEAR: {birthYear}</div>
-            <div>MOVIES: {starData.movieTitles}</div>
+            <div>MOVIEID: {movieData.id}</div>
+            <div>YEAR: {movieData.year}</div>
+            <div>DIRECTOR: {movieData.director}</div>
         </>
     );
 
