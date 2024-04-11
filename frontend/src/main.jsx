492dae12523f73371f2289b8b637a132f3a748dc
Jacob Wong <jacobsunsetbluff@gmail.com>
2024-04-10 21:44:10 -0700

    Implement example React component for practice and some minor formatting changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx'
 import MovieList from '../src/pages/MovieList.jsx'
-import SingleStar from '../src/pages/SingleStar.jsx'
 import SingleMovie from '../src/pages/SingleMovie.jsx'
+import SingleStar from '../src/pages/SingleStar.jsx'
+import Example from '../src/pages/Example.jsx'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import './index.css'
 
@@ -15,10 +16,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
               <Route exact path="/" element={<MovieList/>} />
               <Route exact path="/movielist" element={<MovieList/>} />
               <Route exact path="singlemovie" element={<SingleMovie/>} />
-              <Route path="/singlestar" element={<SingleStar/>} />
-
+              <Route exact path="/singlestar" element={<SingleStar/>} />
+              <Route exact path="/example" element={<Example/>} />
           </Routes>
-
       </BrowserRouter>
   </React.StrictMode>,
 )
