814d7ad83eb24bc9b24185461805333066731e62
andy-phu <andyphu7601@gmail.com>
2024-04-10 17:54:16 -0700

    single star basic completed

--- frontend/src/pages/SingleStar.jsx
+++ frontend/src/pages/SingleStar.jsx
@@ -3,9 +3,9 @@ import {useLocation} from "react-router-dom";
 
 function SingleStar() {
     const [starData, setStarData] = useState({
-         name: "",
-        Year: "N/A",
-        AllMovies: []
+        name: "",
+        birthYear: "N/A",
+        movieTitles: []
 
     });
 
@@ -20,7 +20,6 @@ function SingleStar() {
             try{
                 const response = await fetch(`http://localhost:8080/fabFlix_war/singlestar?name=${encodeURIComponent(name)}`);
 
-                console.log(response);
                 if (!response.ok) {
                     console.error('response is not status 200');
                 }
@@ -28,11 +27,12 @@ function SingleStar() {
 
                 const data = await response.json();
 
+
                 if (mounted){
-                    setStarData(data);
+                    setStarData(data[0]);
                 }
 
-                console.log(data);
+
 
             }catch(error){
                 console.error('Error fetching data: ', error);
@@ -51,9 +51,12 @@ function SingleStar() {
 
     return (
         <>
-            <div>
-                {starData.name}
-            </div>
+            <h1>
+                STAR
+            </h1>
+            <div>NAME: {starData.name}</div>
+            <div>YEAR: {starData.birthYear}</div>
+            <div>MOVIES: {starData.movieTitles}</div>
         </>
     );
 
