bc6b3f2ae1df4ff0fec5f21868fdae4d7b3e1347
andy-phu <andyphu7601@gmail.com>
2024-04-17 00:11:45 -0700

    login starter

---
+++ backend/LoginFilter.java
@@ -0,0 +1,61 @@
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * Servlet Filter implementation class LoginFilter
+ */
+@WebFilter(filterName = "LoginFilter", urlPatterns = "/*")
+public class LoginFilter implements Filter {
+    private final ArrayList<String> allowedURIs = new ArrayList<>();
+
+    /**
+     * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+     */
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        System.out.println("LoginFilter: " + httpRequest.getRequestURI());
+
+        // Check if this URL is allowed to access without logging in
+        if (this.isUrlAllowedWithoutLogin(httpRequest.getRequestURI())) {
+            // Keep default action: pass along the filter chain
+            chain.doFilter(request, response);
+            return;
+        }
+
+        // Redirect to login page if the "user" attribute doesn't exist in session
+        if (httpRequest.getSession().getAttribute("user") == null) {
+            httpResponse.sendRedirect("Login.jsx");
+        } else {
+            chain.doFilter(request, response);
+        }
+    }
+
+    private boolean isUrlAllowedWithoutLogin(String requestURI) {
+        /*
+         Setup your own rules here to allow accessing some resources without logging in
+         Always allow your own login related requests(html, js, servlet, etc..)
+         You might also want to allow some CSS files, etc..
+         */
+        return allowedURIs.stream().anyMatch(requestURI.toLowerCase()::endsWith);
+    }
+
+
+    //these should not need authentication each time these are requested indirectly
+    //essential without auth, login should not need to login to login
+    public void init(FilterConfig fConfig) {
+        allowedURIs.add("Login.jsx");
+        allowedURIs.add("App.jsx");
+        allowedURIs.add("main.jsx");
+        allowedURIs.add("index.html");
+        allowedURIs.add("index.js");
+        allowedURIs.add("fabFlix/login");
+    }
+
+}
