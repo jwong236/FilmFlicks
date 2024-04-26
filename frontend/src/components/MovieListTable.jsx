409283a0d1abee0f3e975b616f2834bb672368d1
Jacob Wong <jacobsunsetbluff@gmail.com>
2024-04-26 15:04:10 -0700

    Hyperlink title and stars to singlemovie and singlestar in movielist

--- frontend/src/components/MovieListTable.jsx
+++ frontend/src/components/MovieListTable.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import {Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Button, useTheme} from '@mui/material';
+import { Link } from 'react-router-dom';
+import {
+    Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Button, useTheme
+} from '@mui/material';
 
-function DataTable({ data, handleAdd}) {
+export default function MovieListTable({ data, handleAdd }) {
     const theme = useTheme();
 
     const headerStyle = {
         color: 'white'
-    }
+    };
 
     return (
         <TableContainer component={Paper}>
@@ -25,11 +28,24 @@ function DataTable({ data, handleAdd}) {
                 <TableBody sx={{ backgroundColor: theme.palette.secondary.light }}>
                     {data.map((movie, index) => (
                         <TableRow key={index}>
-                            <TableCell component="th" scope="row">{movie.title}</TableCell>
+                            <TableCell component="th" scope="row">
+                                <Link to={`/singlemovie?title=${encodeURIComponent(movie.title)}`} style={{ color: theme.palette.primary.main, textDecoration: 'none' }}>
+                                    {movie.title}
+                                </Link>
+                            </TableCell>
                             <TableCell align="right">{movie.year}</TableCell>
                             <TableCell align="right">{movie.director}</TableCell>
                             <TableCell align="right">{movie.genres.join(', ')}</TableCell>
-                            <TableCell align="right">{movie.stars.join(', ')}</TableCell>
+                            <TableCell align="right">
+                                {movie.stars.map((star, idx) => (
+                                    <React.Fragment key={idx}>
+                                        <Link to={`/singlestar?name=${encodeURIComponent(star)}`} style={{ color: theme.palette.primary.main, textDecoration: 'none' }}>
+                                            {star}
+                                        </Link>
+                                        {idx < movie.stars.length - 1 ? ', ' : ''}
+                                    </React.Fragment>
+                                ))}
+                            </TableCell>
                             <TableCell align="right">{movie.rating}</TableCell>
                             <TableCell align="right">
                                 <Button
@@ -51,5 +67,3 @@ function DataTable({ data, handleAdd}) {
         </TableContainer>
     );
 }
-
-export default DataTable;
