6f838772c25d9e869a5667ca4ad3c9523c04e137
Jacob Wong <jacobsunsetbluff@gmail.com>
2024-05-09 19:36:39 -0700

    Update database

--- docker/createtable.sql
+++ docker/createtable.sql
@@ -75,3 +75,10 @@ CREATE TABLE movie_prices (
     price DECIMAL(10,2) NOT NULL,
     FOREIGN KEY (movieId) REFERENCES movies(id)
 );
+
+CREATE TABLE employees (
+                           email VARCHAR(50) PRIMARY KEY,
+                           password VARCHAR(20) NOT NULL,
+                           fullname VARCHAR(100)
+);
+INSERT INTO employees (email, password, fullname) VALUES ('classta@email.edu', 'classta', 'TA CS122B');
